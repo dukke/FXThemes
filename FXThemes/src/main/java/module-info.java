cef3d28bf02aa1ed31806a06d73c16c11a26589f
regice202 <regice202@gmail.com>
2024-07-06 08:20:35 -0700

    Update module-info.java

--- FXThemes/src/main/java/module-info.java
+++ FXThemes/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.pixelduke.fxthemes {
     requires com.sun.jna;
     requires com.sun.jna.platform;
 
+    opens com.pixelduke.window to javafx.graphics;
+
     exports com.pixelduke.window;
 
     exports impl.com.pixelduke.window.win32.windows10 to com.sun.jna;
