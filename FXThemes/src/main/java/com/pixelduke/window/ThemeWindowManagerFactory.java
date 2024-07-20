44f0f05b6cd7f96ca0760257d0f4092b26d005cb
regice202 <regice202@gmail.com>
2024-07-19 22:30:22 -0700

    Update ThemeWindowManagerFactory.java
    Provides more robust handling for different Operating Systems and their major versions.

--- FXThemes/src/main/java/com/pixelduke/window/ThemeWindowManagerFactory.java
+++ FXThemes/src/main/java/com/pixelduke/window/ThemeWindowManagerFactory.java
@@ -4,25 +4,46 @@ import com.sun.jna.Platform;
 import javafx.stage.Window;
 
 public abstract class ThemeWindowManagerFactory {
+    
     public static ThemeWindowManager create() {
         String osName = System.getProperty("os.name");
-        // Check which operating system we're running and return appropriate ThemeWindowManager
-        if (Platform.isWindows()) {
-            if (osName.equals("Windows 10")) {
-                return new Win10ThemeWindowManager();
-            } else {
-                return new Win11ThemeWindowManager();
-            }
-        } else if (Platform.isMac()) {
-            return new MacThemeWindowManager();
-        } else if (Platform.isLinux()) {
-            return new LinuxThemeWindowManager();
-        } else {
-            return new ThemeWindowManager(){
-                public void setDarkModeForWindowFrame(Window window, boolean darkMode){
-                    System.out.println("Warning: Unsupported Window Operating System");
+        int majorVersion = getMajorVersion();
+        
+        // Check which operating system and version we're running and return appropriate ThemeWindowManager
+        switch(Platform.getOSType()){
+            case Platform.WINDOWS:
+                switch(majorVersion){
+                    case 10:
+                        return new Win10ThemeWindowManager();
+                    case 11:
+                        return new Win11ThemeWindowManager();
+                    default:
+                        break;
                 }
-            };
+                break;
+            case Platform.MAC:
+                return new MacThemeWindowManager();
+            case Platform.LINUX:
+                return new LinuxThemeWindowManager();
+            default:
+                break;
         }
+        
+        return new ThemeWindowManager(){
+            public void setDarkModeForWindowFrame(Window window, boolean darkMode){
+                System.out.println("Warning: Unsupported Window Operating System");
+            }
+        };
+    }
+
+    private static int getMajorVersion(){
+        int version = -1;
+        
+        try{
+            version = Integer.parseInt(System.getProperty("os.version").split("\\.")[0]);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return version;
     }
 }
